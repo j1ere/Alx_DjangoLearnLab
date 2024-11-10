ac2a665b137e7b25a1428c3c927f4d7e9ad61db7
j1ere <jeremywambua106@gmail.com>
2024-11-10 23:49:41 +0300

    updted

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register_view, book_list_view  # Import necessary views
 
 urlpatterns = [
     path('books/', views.book_list_view, name='book_list'),  # URL for the function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
 
 
-    path('register/', views.register_view, name='register'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('books/', views.book_list_view, name='book_list'),  # Example URL for redirection
+    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('books/', book_list_view, name='book_list'),  # Example URL for redirection after login
     # Add other views and URLs as needed
 
 
