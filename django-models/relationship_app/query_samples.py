d4b4b59057285ef5feb6f181f2cd20de69bc5fe3
j1ere <jeremywambua106@gmail.com>
2024-11-10 23:27:00 +0300

    query samples edit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,16 +1,25 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
+# Query all books by a specific author using filter
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
+    try:
+        author = Author.objects.get(name=author_name)
+        return Book.objects.filter(author=author)
+    except Author.DoesNotExist:
+        return []
 
-# List all books in a library
+# List all books in a library using filter
 def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()  # No change needed here since it's already fetching related objects
+    except Library.DoesNotExist:
+        return []
 
-# Retrieve the librarian for a library
+# Retrieve the librarian for a library using filter
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian  # No change needed here as this is a one-to-one relationship
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
