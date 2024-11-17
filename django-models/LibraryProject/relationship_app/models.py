3ca5055117b8d82f657981ed7cb70f4426930745
j1ere <jeremywambua106@gmail.com>
2024-11-17 23:19:30 +0300

    further correction

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,6 +9,12 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# models.py
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
