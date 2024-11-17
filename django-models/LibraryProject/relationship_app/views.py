4a37ff2c0f3e51e0886469f75a7b09016d173075
j1ere <jeremywambua106@gmail.com>
2024-11-17 23:26:39 +0300

    sdf

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,8 +19,8 @@ def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 
 # Admin view (Only accessible by Admin users)
-@user_passes_test(is_admin)
-def Admin_view(request):
+@user_passes_test(Admin)
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 def is_librarian(user):
