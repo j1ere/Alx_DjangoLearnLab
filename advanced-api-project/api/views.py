e8c14f30b056172d6a292b43b1d664418d37e5ed
j1ere <jeremywambua106@gmail.com>
2024-12-01 14:56:04 +0300

    from django_filters import rest_framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .permissions import IsAdminOrReadOnly
 from rest_framework.filters import SearchFilter, OrderingFilter
-
+from django_filters import rest_framework 
+#from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 # Retrieve all books
 
@@ -19,14 +20,19 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 
 class BookListView(ListAPIView):
     """
-    Simplified view with only search and ordering.
+    View to list all books with filtering, searching, and ordering capabilities.
+    - Filtering: Use query parameters (e.g., `?title=Sample` or `?publication_year=2023`).
+    - Searching: Use `?search=<term>` to search title or author name.
+    - Ordering: Use `?ordering=title` or `?ordering=-publication_year` to sort results.
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
-    ordering = ['title']
+    ordering = ['title']  # Default ordering
 
 # Retrieve a specific book by ID
 class BookDetailView(RetrieveAPIView):
