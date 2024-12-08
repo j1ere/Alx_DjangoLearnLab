dac5bb6066095921bc2ec8de801c0e9d1addbbdf
j1ere <jeremywambua106@gmail.com>
2024-12-08 08:39:13 +0300

    url pattern fix for comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
-    path('blog/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
-    path('comments/edit/<int:pk>/', views.CommentUpdateView.as_view(), name='edit_comment'),
-    path('comments/delete/<int:pk>/', views.CommentDeleteView.as_view(), name='delete_comment'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
 ]
