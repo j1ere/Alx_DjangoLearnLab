8bd17648dd863c8f4893bedb52b3dc8e2d200ed4
j1ere <jeremywambua106@gmail.com>
2024-12-08 07:02:52 +0300

    url updates

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home_view, name="home"),
-    path('post/', views.posts_view, name="posts"),
+    path('posts/', views.posts_view, name="posts"),
     path('login/', views.login_view, name="login"),
     path('register/', views.register_view, name="register"),
     path('logout/', views.logout_view, name="logout"),
     path('profile/', views.profile_view, name="profile"),
-    path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/edit/<int:pk>/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('posts/delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
